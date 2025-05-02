9610940a352e169f64cf153e3a738c19408ba409
Carturo8 <carturo3883@gmail.com>
2025-05-02 09:57:48 -0500

    Update

--- M1S2_Ejercicios_For.py
+++ M1S2_Ejercicios_For.py
@@ -6,10 +6,12 @@
 # lista_numeros:list[int] = [int(x) for x in numeros]
 # lista_numeros_mayores:list[int] = []
 # contador:int = 0
+
 # for numero in lista_numeros:
 #     if numero >= 1000:
 #         lista_numeros_mayores.append(numero)
 #         contador += 1
+
 # print(f"Los números mayores o iguales a 1000 son: {contador} --> {lista_numeros_mayores}")
 
 # 2. Cálculo de áreas de triángulos
@@ -20,10 +22,9 @@
 # base_altura:str = input("Ingrese los pares de datos (base,altura) separados por comas: ")  # ejemplo: 30,40,20.5,30.5,20.5
 # lista_base_altura:list = base_altura.split(",")
 # lista_base_altura = [round(float(x.strip()), 2) for x in lista_base_altura]  # convertimos a float
-# contador_superficie_mayor_12:float = 0
-#
-# # Creamos los pares
 # datos_triangulo = []
+# contador_superficie_mayor_12:float = 0
+
 # for i in range(0, len(lista_base_altura), 2):  # saltamos de 2 en 2
 #     if i + 1 < len(lista_base_altura):  # verificamos que exista el segundo elemento del par
 #         superficie = round((lista_base_altura[i] * lista_base_altura[i + 1]) / 2, 2)
@@ -34,3 +35,32 @@
 #
 # print(f"Pares de datos: {datos_triangulo}")
 # print(f"Cantidad de triángulos con superficie mayor a 12: {contador_superficie_mayor_12}")
+
+# Puntos en el plano cartesiano
+"""Escribir un programa que pida ingresar coordenadas (x, y) y determine cuántos puntos se encuentran en cada cuadrante 
+(I, II, III, IV). Nota: Al inicio, el programa debe solicitar la cantidad de puntos a procesar."""
+
+coordenadas:str = input("Ingrese las coordenadas (x, y) separadas por comas: ")
+coordenadas:str = coordenadas.split(",")
+contador_cuadrante_1:int = 0
+contador_cuadrante_2:int = 0
+contador_cuadrante_3:int = 0
+contador_cuadrante_4:int = 0
+
+for i in range(0, len(coordenadas), 2):
+    if i + 1 < len(coordenadas):  # verificamos que exista el segundo elemento del par
+        x = float(coordenadas[i])
+        y = float(coordenadas[i + 1])
+        if x > 0 and y > 0:
+            contador_cuadrante_1 += 1
+        elif x < 0 and y > 0:
+            contador_cuadrante_2 += 1
+        elif x < 0 and y < 0:
+            contador_cuadrante_3 += 1
+        else:
+            contador_cuadrante_4 += 1
+
+print(f"""El número de puntos en el cuadrante I es: {contador_cuadrante_1}
+El número de puntos en el cuadrante II es: {contador_cuadrante_2}
+El número de puntos en el cuadrante III es: {contador_cuadrante_3}
+El número de puntos en el cuadrante IV es: {contador_cuadrante_4}""")
