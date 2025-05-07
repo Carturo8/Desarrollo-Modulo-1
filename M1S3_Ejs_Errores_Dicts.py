161d1a036b71861f0a0f40ed6db59630de60dd6c
Carturo8 <carturo3883@gmail.com>
2025-05-07 12:56:51 -0500

    Update

--- M1S3_Ejs_Errores_Dicts.py
+++ M1S3_Ejs_Errores_Dicts.py
@@ -1,5 +1,5 @@
 """El siguiente código intenta sumar los precios de un diccionario, pero no funciona.
-Encuentra el error y corrígelo para que calcule correctamente el total. Python"""
+Encuentra el error y corrígelo para que calcule correctamente el total."""
 
 precios = {"manzana": 1.5, "banana": 0.8, "pera": 1.2}
 total = 0
